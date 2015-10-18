7f84f3f28ef1d4cfd8962077023cb6e42d1b2cdb
nazmiaydin <nazmiaydin@yandex.com>
2015-10-18 22:55:36 +0300

    increment and decrement add player and remove player

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,7 @@ PlayersList = new Mongo.Collection('players');
 if (Meteor.isClient) {
     Template.leaderboard.helpers({
         'player': function() {
-            return PlayersList.find()
+            return PlayersList.find({}, {sort: {score: -1, name: 1}})
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -11,6 +11,10 @@ if (Meteor.isClient) {
             if(playerId == selectedPlayer) {
                 return "selected"
             }
+        },
+        'showSelectedPlayer': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            return PlayersList.findOne(selectedPlayer)
         }
     });
 
@@ -18,8 +22,32 @@ if (Meteor.isClient) {
        'click .player': function(){
            var playerId = this._id;
            Session.set('selectedPlayer', playerId);
+       },
+       'click .increment': function(){
+           var selectedPlayer = Session.get('selectedPlayer');
+           PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+       },
+        'click .decrement': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+        },
+        'click .remove': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.remove(selectedPlayer)
+        }
+    });
 
-       }
+    Template.addPlayerForm.events({
+       'submit form': function(event){
+           event.preventDefault();
+           var playerNameVar = event.target.playerName.value;
+           PlayersList.insert({name: playerNameVar, score:0});
+
+       },
+        'click a': function(event){
+            event.preventDefault();
+        }
+
     });
 }
 if (Meteor.isServer) {
